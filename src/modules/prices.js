5c226ef606c67e0c30b0a6d9b2b2f3b3762a2642
Алексей Горощеня <lastpaladin13@gmail.com>
2024-01-22 12:33:40 +0300

    finish third html

--- src/modules/prices.js
+++ src/modules/prices.js
@@ -2,7 +2,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 export const prices = () => {
 
-    const prices = new Swiper('.prices__cards', {
+    const prices = new Swiper('.prices__cards_four', {
         slidesPerView: 1.2,
         spaceBetween: 40,
         
@@ -25,6 +25,34 @@ export const prices = () => {
             },
             991.98: {
                 slidesPerView: 4,
+            },
+        }
+
+    });
+
+    const pricesContext = new Swiper('.prices__cards_three', {
+        slidesPerView: 1.2,
+        spaceBetween: 40,
+        
+        modules: [Navigation, Pagination],
+       
+        navigation: {
+            nextEl: '.prices__cards-button-next',
+            prevEl: '.prices__cards-button-prev',
+        },
+        pagination: {
+            el: '.prices__cards-pagination',
+            clickable: true,
+            bulletActiveClass: 'pagination-bullet-active',
+            bulletClass: 'pagination-bullet',
+          },
+        breakpoints: {
+
+            767.98: {
+                slidesPerView: 2.2,
+            },
+            991.98: {
+                slidesPerView: 3,
             },
         }
 
